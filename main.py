68333088f525a14c2cc8e08032abee1e07f4eaa2
Opeoluwa Fatunmbi <101920613+opeoluwaaaa@users.noreply.github.com>
2022-09-28 23:11:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ from wallstreet import Stock, Call, Put
 import datetime
 
 
-class StockPriceChecker:
 
+
+
+
+class StockPriceChecker:
     
-    def __init__(self, first_name, last_name, intrument) -> None:
+
+
+    def __init__(self, first_name, last_name) -> None:
         self.first_name = first_name
         self.last_name = last_name
-        self.instrument = intrument
+        # self.instrument = intrument
 
     def full_name(self):
-        self.first_name = input("FIRST NAME: ")
-        self.last_name = input("LAST NAME: ")
+        # self.first_name = input("FIRST NAME: ")
+        # self.last_name = input("LAST NAME: ")
         return f'{self.first_name} {self.last_name}'
     
-
-    def get_price_of_instrument(self):
-        s = Stock('AAPL')
-        print(s.price)
-
-
-
+    # @classmethod
+    def get_price_of_instrument(self): 
+        stock_name = input("What is the stock? ")  
+        stock_price = Stock(stock_name)
+        return stock_price.price
 
 
 
-First_Trader = StockPriceChecker("John","Doe","")
+
+
+
+
+First_Trader = StockPriceChecker("John","Doe")
+print(First_Trader.get_price_of_instrument())
